4d28014d77bd0754140439a3625365e19c0ac7f1
lqrhy3 <m1xst99@yandex.ru>
2022-10-25 12:38:10 +0300

    update smoke tests

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,16 +1,26 @@
+import torch.cuda
+
 from torch_utils.ops import filtered_lrelu
 from torch_utils.ops import upfirdn2d
 from torch_utils.ops import bias_act
 
 
 def test_filtered_lrelu_compilability():
-    assert filtered_lrelu._init()
+    if torch.cuda.is_available():
+        assert filtered_lrelu._init()
+    else:
+        assert True  # no compilation needed if there is no CUDA
 
 
 def test_upfirdn2d_compilability():
-    assert upfirdn2d._init()
+    if torch.cuda.is_available():
+        assert upfirdn2d._init()
+    else:
+        assert True  # no compilation needed if there is no CUDA
 
 
 def test_bias_act_compilability():
-    assert bias_act._init()
-
+    if torch.cuda.is_available():
+        assert bias_act._init()
+    else:
+        assert True  # no compilation needed if there is no CUDA
